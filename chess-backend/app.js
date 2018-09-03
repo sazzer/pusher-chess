950e138e463096c390474a4216abc4680b50839e
Graham Cox <graham@grahamcox.co.uk>
2018-09-03 17:01:13 +0100

    Final setup tweaks

--- chess-backend/app.js
+++ chess-backend/app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -13,8 +10,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
